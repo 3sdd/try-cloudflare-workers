53fd2cc081d4f20683ec9e9e962e64cdb0325212
3sdd <56486103+3sdd@users.noreply.github.com>
2024-03-01 22:55:42 +0900

    router試してみた

--- src/index.js
+++ src/index.js
@@ -7,9 +7,24 @@
  *
  * Learn more at https://developers.cloudflare.com/workers/
  */
+import { Router } from 'itty-router';
+
+const router = Router();
+
+router.get('/', () => {
+	return new Response('Hello world');
+});
+
+router.get('/example/:test', ({ params }) => {
+	return new Response(`<p>${params.test}</p>`, {
+		headers: {
+			'Content-Type': 'text/html',
+		},
+	});
+});
+
+router.all('*', () => new Response('404 not found', { status: 404 }));
 
 export default {
-	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
-	},
+	fetch: router.fetch,
 };
